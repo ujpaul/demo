4b20b30dd20a9c2f9d2fdb51c750c7f7c0ab7f4c
uwimana jean paul <uwimanajeanpaul3@gmail.com>
2022-10-22 09:36:45 +0200

    Updates

---
+++ precache-manifest.cf07f4270adbca05264125aa7891205f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf7662bb02f344cec2a6",
+    "url": "/demo/static/css/main.15004d75.chunk.css"
+  },
+  {
+    "revision": "cf7662bb02f344cec2a6",
+    "url": "/demo/static/js/main.cb6118ba.chunk.js"
+  },
+  {
+    "revision": "439e804dccec2cbef2be",
+    "url": "/demo/static/js/runtime~main.69626317.js"
+  },
+  {
+    "revision": "6d7022e2538ac3ad4afa",
+    "url": "/demo/static/css/2.838d6985.chunk.css"
+  },
+  {
+    "revision": "6d7022e2538ac3ad4afa",
+    "url": "/demo/static/js/2.dce03478.chunk.js"
+  },
+  {
+    "revision": "e8353ab84e98a3f809b4",
+    "url": "/demo/static/js/3.25923653.chunk.js"
+  },
+  {
+    "revision": "60b7e01fde850094d7f21f43427bd962",
+    "url": "/demo/index.html"
+  }
+];
